19caf343595097667a3ccfbe9be388882bb52e7f
rleongPd5 <rleong00@stuy.edu>
2019-03-06 11:27:43 -0500

    Added methods and import

---
+++ USACO.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class USACO{
+  public static int bronze(String filename){
+    File f = new File(filename);
+    Scanner s = new Scanner(f);
+
+    return 0; //dummy
+  }
+
+  public static int silver(String filename){
+    return 0; //dummy
+  }
+}
